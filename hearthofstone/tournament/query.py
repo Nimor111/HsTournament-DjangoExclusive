6f22b5c2e6d2237cc30b3d976c313c15954f3ced
Nimor111 <georgi.bojinov@hotmail.com>
2017-05-11 00:12:06 +0300

    Tournament detail can be accessed

--- hearthofstone/tournament/query.py
+++ hearthofstone/tournament/query.py
@@ -4,4 +4,4 @@ from django.db import models
 class TournamentQuerySet(models.QuerySet):
 
     def get_active_tournaments(self):
-        return self.filter(active=True)
+        return self.filter(active=True)[:2]
