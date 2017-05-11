99d08d0f0548daa39c2c5de4d1382bbd0d5d69f8
Nimor111 <georgi.bojinov@hotmail.com>
2017-05-11 12:34:48 +0300

    Try deploy to heroku

--- hearthofstone/hearthofstone/settings.py
+++ hearthofstone/hearthofstone/settings.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -69,22 +71,6 @@ WSGI_APPLICATION = 'hearthofstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tournament',
-        'USER': 'tournamentuser',
-        'PASSWORD': 'tournamentpass',
-        'HOST': 'localhost',
-        'TEST': {
-            'NAME': 'test_hearthofstone',
-            'USER': 'tournamentuser',
-        },
-        'PORT': '',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -131,3 +117,16 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_REDIRECT_URL = '/'
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
